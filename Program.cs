e66d07d7b25a92ba554874687d0197983052f53e
SerhatAker <81346603+serhataker@users.noreply.github.com>
2024-08-07 00:53:17 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using WorkerOfCompany;
+using static WorkerOfCompany.ProjectManager;
+
+namespace Worker
+{
+    class WorkersCompany
+    {
+
+
+        static void Main(string[] args)
+        {
+            Workers projectManager = new ProjectManager();// we created instance projectmanager we can't the workers abstarct class but we can do it ProjectManager
+
+            projectManager.job("Software PM");//we call the from the aabstract method implemneting method.
+            //information property inpu for the PM object
+            projectManager.Name = "Hakan";
+            projectManager.Surname = "Mir";
+            projectManager.Department = "Project";
+            projectManager.Info();// call the constructor gived the by property
+
+            Workers softwareDev = new SoftwareDeveloper{ Department = "Developer", Name = "Hasan", Surname = "Hayır" // another implemneting class process
+            };
+            softwareDev.job("Developer");
+            softwareDev.Info();
+
+            Workers saler = new Saler { Department = "Sales", Surname = "Ogeday", Name = "Çınar" }; // another implementing class process
+            saler.job("Saler");
+            saler.Info();
+        }
+    }
+
+}
---
+++ Workers.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using static WorkerOfCompany.ProjectManager;
+
+namespace WorkerOfCompany
+{
+    public abstract class Workers // we created the abstract class 
+    {
+        // created the property for the abstarct method
+        public  string Name { get; set; } // 
+        public string Surname { get; set; }
+
+        public string Department { get; set; }
+
+        public abstract void job(string jobs); // created the abstarct method
+        public void Info() // we optional create the cunstroctor method for the information
+        {
+            Console.WriteLine($"Name:{Name}");
+            Console.WriteLine($"Surname:{Surname}");
+            
+            Console.WriteLine($"Departmant:{Department}");
+        }
+
+    }
+
+    public class ProjectManager : Workers // we create the projectmanager class and we inherited the workers class
+    {
+        public override void job(string jobs) //  we overide job nethod for the PO
+        {
+            Console.WriteLine($"i am working to {jobs}");
+        }
+
+        public class SoftwareDeveloper : Workers// we create the softwaredeveloper class and we inherited the workers class
+        {
+            public override void job(string jobs) //  we overide job nethod for the Develpoer
+            {
+                Console.WriteLine($"i am working to {jobs}");
+            }
+
+        }
+        public class Saler : Workers //we create the Saler class and we inherited the workers class
+        {
+            public override void job(string jobs) //  we overide job nethod for the Saler
+            {
+                Console.WriteLine($"i am working to {jobs}");
+            }
+
+        }
+    }
+}
